2fe7adf2d0cbf1511d5b9d7d9074013a46a3b8de
Alexis Guzmán <spartanthealexvro@gmail.com>
2026-02-25 11:39:56 -0600

    [FIX]: Agregando home como punto de entrada para verificar el renderizado

--- app/src/main/java/com/uriel/musicjam/core/navegation/AppNavegation.kt
+++ app/src/main/java/com/uriel/musicjam/core/navegation/AppNavegation.kt
@@ -4,16 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.composable
+import com.uriel.musicjam.features.auth.presentation.screens.LoginScreen
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
-        startDestination = AppScreens.Home.route
+        startDestination = AppScreens.LogIn.route
     ) {
         composable(AppScreens.LogIn.route) {
-            //LogInScreen(navController = navController)
+            LoginScreen(
+                onNavigateToHome = {},
+                onNavigateToRegister = {}
+            )
         }
         composable(AppScreens.SignUp.route) { //
         // SignUpScreen(navController = navController)
